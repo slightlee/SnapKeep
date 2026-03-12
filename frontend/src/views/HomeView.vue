96eef1dd74c6449ef9d9b138142dd38b848368b8
demain_lee <lmm_work@163.com>
2026-03-12 23:53:24 +0800

    feat(logo): 添加SnapKeep logo标志和相关资源

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -5,19 +5,7 @@
         <div class="header-logo" :title="resolvedSiteName">
           <img v-if="siteIconIsUrl" :src="resolvedSiteIcon" class="logo-img" alt="" />
           <span v-else-if="siteIconText" class="logo-emoji">{{ siteIconText }}</span>
-          <svg
-            v-else
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            aria-hidden="true"
-          >
-            <path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z" />
-            <polyline points="13 2 13 9 20 9" />
-          </svg>
+          <img v-else src="/branding/snapkeep-logo-mark.svg" class="logo-img" alt="" />
           <span>{{ resolvedSiteName }}</span>
         </div>
 
